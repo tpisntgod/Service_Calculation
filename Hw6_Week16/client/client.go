6582532fb9c8ea2ab821db4ebb1887bef068fcf4
toupiOfRivia <453040945@qq.com>
2018-01-04 10:46:12 +0800

    finish sync and async method

---
+++ Hw6_Week16/client/client.go
@@ -0,0 +1,84 @@
+package client
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	min     = 1000000
+	max     = 9999999
+	urlhead = "http://api.fanyi.baidu.com/api/trans/vip/translate"
+	from    = "en"
+	to      = "zh"
+	appid   = "20180103000111810"
+	key     = "7N1Csy2YSMhU0JcOjCIf"
+)
+
+type resultJSON struct {
+	From        string              `json:"from"`
+	To          string              `json:"to"`
+	TransResult []map[string]string `json:"trans_result"`
+}
+
+func getSalt() string {
+	rand.Seed(time.Now().Unix())
+	t := min + rand.Intn(max-min)
+	return strconv.Itoa(t)
+}
+
+func getMD5(s string) string {
+	md5Obj := md5.New()
+	md5Obj.Write([]byte(s))
+	digest := md5Obj.Sum(nil)
+	return hex.EncodeToString(digest)
+}
+
+func getURL(q string) string {
+	salt := getSalt()
+	url := urlhead + "?q=" + q + "&from=" + from + "&to=" + to +
+		"&appid=" + appid + "&salt=" + salt + "&sign=" + getMD5(appid+q+salt+key)
+	return url
+}
+
+//HTTPGetSync 分别使用同步和异步方法向百度翻译发送5个翻译请求
+func HTTPGetSync(q string) string {
+	res, err := http.Get(getURL(q))
+	CheckErr(err)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	CheckErr(err)
+	var result resultJSON
+	if err = json.Unmarshal(body, &result); err == nil {
+		return fmt.Sprintln(result.TransResult[0]["src"], ":", result.TransResult[0]["dst"])
+	}
+	return q + "failed when parsing json response!\n"
+}
+
+//HTTPGetAsync 使用 go HTTPClient 实现图 6-2 的 Naive Approach
+func HTTPGetAsync(q string, ch chan string) {
+	res, err := http.Get(getURL(q))
+	CheckErr(err)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	CheckErr(err)
+	var result resultJSON
+	err = json.Unmarshal(body, &result)
+	CheckErr(err)
+	transResult := result.TransResult[0]["dst"]
+	ch <- transResult
+}
+
+//CheckErr check err
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- Hw6_Week16/main.go
+++ Hw6_Week16/main.go
@@ -1,14 +1,43 @@
 package main
 
-func syncRequest() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/tpisntgod/Service_Calculation/Hw6_Week16/client"
+)
+
+func syncRequest() {
+	startTime := time.Now()
+	r1 := client.HTTPGetSync("love")
+	r2 := client.HTTPGetSync("learning")
+	r3 := client.HTTPGetSync("go")
+	r4 := client.HTTPGetSync("language")
+	r5 := client.HTTPGetSync("thank")
+	fmt.Printf("%s\n%s%s%s%s%s", "Translate Result:", r1, r2, r3, r4, r5)
+	fmt.Println("Time consumed:", time.Since(startTime))
 }
 
 func asyncRequest() {
-
+	startTime := time.Now()
+	ch := make(chan string)
+	go client.HTTPGetAsync("love", ch)
+	go client.HTTPGetAsync("learning", ch)
+	go client.HTTPGetAsync("go", ch)
+	go client.HTTPGetAsync("language", ch)
+	go client.HTTPGetAsync("thank", ch)
+	fmt.Println("translate result:\n", <-ch, <-ch, <-ch, <-ch, <-ch)
+	fmt.Println("Time consumed:", time.Since(startTime))
 }
 
 func main() {
+	fmt.Printf("%s\n\n", "分别使用同步和异步方法向百度翻译发送5个翻译请求")
+	fmt.Printf("%s\n", "使用 go HTTPClient 实现图 6-2 的 Naive Approach")
+
 	syncRequest()
+
+	fmt.Printf("%s\n\n", "------------我是分割线------------")
+	fmt.Printf("%s\n", "为每个 HTTP 请求设计一个 goroutine ，利用 Channel 搭建基于消息的异步机制，实现图 6-3")
+
 	asyncRequest()
 }
